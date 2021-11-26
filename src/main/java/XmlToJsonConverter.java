da9b723b44089e35a7bcd2db2b336d7bbfa19e3d
kolegran <kolegran@gmail.com>
2021-11-26 23:55:39 +0200

    Added children handling with recursion

--- src/main/java/XmlToJsonConverter.java
+++ src/main/java/XmlToJsonConverter.java
@@ -1,5 +1,3 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -9,35 +7,39 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class XmlToJsonConverter {
 
     public static void main(String[] args) {
-        ClassLoader classLoader = XmlToJsonConverter.class.getClassLoader();
-        String fileName = classLoader.getResource("input.xml").getFile();
-        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        JSONObject resultArray = new JSONObject();
+        final ClassLoader classLoader = XmlToJsonConverter.class.getClassLoader();
+        final String fileName = classLoader.getResource("input.xml").getFile();
+        final DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
         try {
-            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-            Document document = docBuilder.parse(new File(fileName));
-            NodeList nodeList = document.getElementsByTagName("*");
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    JSONObject tempObject = new JSONObject();
-                    tempObject.put(node.getNodeName(), node.getTextContent());
-                    resultArray.put(tempObject.toString(), " ");
-                }
-            }
-            File result = new File("result.json");
-            FileWriter writer = new FileWriter(result);
-            writer.write(resultArray.toString());
-            writer.close();
-            System.out.println(resultArray.toString());
+            final DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            final Document document = docBuilder.parse(new File(fileName));
+            final Node firstChild = document.getFirstChild();
+
+            System.out.println(firstChild.getNodeName());
+            handleChildren(firstChild);
+
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void handleChildren(Node node) {
+        if (!node.hasChildNodes()) {
+            return;
+        }
+
+        NodeList childNodes = node.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(item.getNodeName());
+                handleChildren(item);
+            }
         }
     }
 }
