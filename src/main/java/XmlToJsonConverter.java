05dee228fe1510a99a97ede77de7f4125a58bc0a
Pawawudaf <oleksandr.kukotin@gmail.com>
2021-11-27 22:33:09 +0200

    To intermediate review

--- src/main/java/XmlToJsonConverter.java
+++ src/main/java/XmlToJsonConverter.java
@@ -1,3 +1,5 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -7,6 +9,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class XmlToJsonConverter {
@@ -20,15 +23,21 @@ public class XmlToJsonConverter {
             final Document document = docBuilder.parse(new File(fileName));
             final Node firstChild = document.getFirstChild();
 
-            System.out.println(firstChild.getNodeName());
-            handleChildren(firstChild);
+            JSONArray resultJson = new JSONArray();
+            handleChildren(firstChild, resultJson);
+            System.out.println(resultJson);
+            File result = new File("result.json");
+            FileWriter fileWriter = new FileWriter(result);
+            fileWriter.write(resultJson.toString());
+            fileWriter.close();
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void handleChildren(Node node) {
+    private static void handleChildren(Node node, JSONArray jsonArray) {
+        JSONObject tempObject = new JSONObject();
         if (!node.hasChildNodes()) {
             return;
         }
@@ -37,9 +46,13 @@ public class XmlToJsonConverter {
         for (int i = 0; i < childNodes.getLength(); i++) {
             Node item = childNodes.item(i);
             if (item.getNodeType() == Node.ELEMENT_NODE) {
-                System.out.println(item.getNodeName());
-                handleChildren(item);
+                tempObject.put(item.getNodeName(), item.getLastChild().getTextContent());
+                System.out.println(item.getNodeName() + " : " + item.getFirstChild().getTextContent());
+                handleChildren(item, jsonArray);
             }
+        }
+        if (!tempObject.isEmpty()){
+            jsonArray.put(tempObject);
         }
     }
 }
